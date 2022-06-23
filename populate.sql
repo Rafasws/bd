172be197dc5e13bfb386f36b3d9f79900db17244
Rafasws <rafael.gaspar.ferreira007@gmail.com>
2022-06-23 23:33:19 +0100

    add old code

--- populate.sql
+++ populate.sql
@@ -340,18 +340,25 @@ CREATE TRIGGER trigger_delete_from_category
 BEFORE DELETE ON category
 FOR EACH ROW EXECUTE PROCEDURE trigger_delete_from_category();
 
+
 CREATE OR REPLACE FUNCTION trigger_add_from_has_other()
 RETURNS TRIGGER AS
 $$
+    DECLARE new_sub_category varchar(80);
+            new_super_category varchar(80);
     BEGIN
-        INSERT INTO category VALUES (NEW.category);
-        IF NEW.super_category IN(
-            SELECT simple_name FROM simple_category;
-        )
+        new_sub_category = NEW.category;
+        new_super_category = NEW.super_category;
+        INSERT INTO category VALUES (new_sub_category);
+        INSERT INTO simple_category VALUES (new_sub_category);
+        IF NOT EXISTS(
+            SELECT * 
+            FROM super_category
+            WHERE new_super_category = super_category.super_name)
         THEN 
             DELETE FROM simple_category
-            WHERE simple_name = NEW.super_category;
-            INSERT INTO super_category VALUES (NEW.super_category);
+            WHERE simple_name = new_super_category;
+            INSERT INTO super_category VALUES (new_super_category);
         END IF;
     END;
 $$ LANGUAGE plpgsql; 

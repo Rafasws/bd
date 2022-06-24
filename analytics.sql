58195ded6fced4a3f165bfcce67586c8ad605798
1-0-GO <martinsfrancisco01@gmail.com>
2022-06-24 18:32:37 +0100

    Update analytics.sql

--- analytics.sql
+++ analytics.sql
@@ -14,27 +14,10 @@
 SELECT day_of_week, council, SUM(units) AS sales
 FROM Sales
 WHERE 
-    year >= 1999 AND quarter >= 1 AND day_of_month > 1
+    year >= 1999 AND quarter >= 1
     AND
-    year <= 2000 AND quarter <= 3 AND day_of_month < 20
-GROUP BY 
-    CUBE(day_of_week, council);
-
-/*
-    ------------------------------------------------------------------------------
-    #                       (V2)
-    ------------------------------------------------------------------------------
-*/
-
-SELECT day_of_week, council, SUM(units) AS sales
-FROM Sales NATURAL JOIN replenishment_event
-WHERE
-    instant >= '1999-02-08 00:00:00'
-    AND
-    instant <= '1999-03-08 00:00:00'
-GROUP BY 
-    CUBE(day_of_week, council);
-
+    year <= 2000 AND quarter <= 3
+GROUP BY CUBE(day_of_week, council);
 
 /*
     ------------------------------------------------------------------------------
@@ -47,6 +30,5 @@ SELECT council, cat, day_of_week, SUM(units) AS sales
 FROM Sales NATURAL JOIN product
 WHERE 
     district = 'Lisboa'
-GROUP BY 
-    CUBE(council, cat, day_of_week);
+GROUP BY CUBE(council, cat, day_of_week);
 
